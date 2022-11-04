6bea9f6045789d2e4ccd3b1817de101aff1412f7
Booeook <booeook@gmail.com>
2022-11-04 11:41:12 +0800

    ORZ

--- Assets/Scripts/Fight/FightPlayer_Turn.cs
+++ Assets/Scripts/Fight/FightPlayer_Turn.cs
@@ -7,6 +7,14 @@ public class FightPlayerTurn : FightUnit
     public override void Init()
     {
         //base.Init();
+        Debug.Log("playerTime");
+        UIManager.Instance.ShowTip("玩家回合", Color.green, delegate()
+        {
+            //抽牌
+            Debug.Log("抽牌");
+            UIManager.Instance.GetUI<FightUI>("FightUI").CreateCardItem(4);//抽4张
+            UIManager.Instance.GetUI<FightUI>("FightUI").UpdateCardItemPos();
+        });
     }
 
     public override void OnUpdate()
